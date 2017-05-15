efe66c38a13d052c3b8d3f5f7a88e038edb72de7
johnflanigan <johnrflanigan@gmail.com>
2017-05-15 00:51:53 -0400

    Moved static folder and changed settings.py

--- qracker/settings.py
+++ qracker/settings.py
@@ -129,11 +129,13 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_DIR = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [STATIC_DIR, ]
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, 'static'),
+]
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
