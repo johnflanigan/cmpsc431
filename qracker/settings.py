7ad0b80e5ac4cb5edde31baf83ba6d446c9e099d
johnflanigan <johnrflanigan@gmail.com>
2017-05-15 00:43:07 -0400

    Changed base directory back and added import

--- qracker/settings.py
+++ qracker/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
